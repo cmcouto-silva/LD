4b10b8b8958802bed5a7177885cce8c9045a6b6f
Cainã Max Couto-Silva <cmcouto.silva@gmail.com>
2018-01-13 17:37:10 -0200

    update R/internal_functions.R

---
+++ R/internal_functions.R
@@ -0,0 +1,8 @@
+#' @export
+
+cap <- function(word) {
+  sapply(word, function(x) {
+    vec <- unlist(strsplit(x, split = ""))
+    paste0(c(toupper(vec)[1], vec[2:length(vec)]), collapse = "")
+  })
+}
